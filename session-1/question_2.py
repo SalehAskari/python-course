d1a87d83528f60ae93b6a9ef1683ebc7167d68f5
SalehAskari <salehaskari.4992@gmail.com>
2025-11-28 22:19:42 +0330

    edite question-2

--- session-1/question_2.py
+++ session-1/question_2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
-price = float(input('enter price: '))
+price = float(input("enter price: "))
 
-tip = float(price * (18/100))
-tax = float(price * (7/100))
+tip = float(price * (18 / 100))
+tax = float(price * (7 / 100))
 
 total = price + tax + tip
 
-print(f'tip: {tip}\ttax: {tax1}\ttotal: {total}')
+print(f"tip: {tip}\ttax: {tax}\ttotal: {total}")
+
